c5845048b0356972a55fed9117a1147004cd071b
gmatheus66 <gmatheus66@hotmail.com>
2019-10-26 16:04:44 -0300

    GH-3 Criação da model

--- checkup/app/User.php
+++ checkup/app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function checkup(){
+        return $this->hasMany('App\Checkup');
+    }
 }
