fd9f5edf8256b1a7f783f40cf7f517fb895347da
RenissonSilva <renissonx@gmail.com>
2019-10-25 22:45:28 -0300

    GH-2 Modificação da migration de user

--- checkup/database/migrations/2014_10_12_000000_create_users_table.php
+++ checkup/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->enum('blood_type', ['A', 'B', 'AB', 'O']);
-            $table->enum('type', ['user', 'medic']);
+            $table->enum('type', ['patient', 'medic']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
