0fa77f1b6c70c73e65789891a8cd7dd131cdaec8
gmatheus66 <gmatheus66@hotmail.com>
2019-10-26 16:00:26 -0300

    GH-3 Crição da factory

--- checkup/database/factories/UserFactory.php
+++ checkup/database/factories/UserFactory.php
@@ -17,11 +17,14 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $hash = Hash::make($faker->lexify('ipi2019'));
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => $hash, // ipi2019
         'remember_token' => Str::random(10),
+        'blood_type' => $faker->randomElement(['A', 'B', 'AB', 'O']),
+        'type' => $faker->randomElement(['patient' , 'medic'])
     ];
 });
