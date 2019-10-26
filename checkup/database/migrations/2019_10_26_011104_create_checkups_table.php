efb8ea7cecebeed11c5cb71654509d3ce206cf40
RenissonSilva <renissonx@gmail.com>
2019-10-25 22:44:22 -0300

    GH-2 Criação da migration do checkup

---
+++ checkup/database/migrations/2019_10_26_011104_create_checkups_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('data_checkup');
+            $table->float('peso',3,1);
+            $table->float('altura',3,2);
+            $table->text('pressao');
+            $table->integer('glicose');
+            $table->float('colesterol_LDL',3,1);
+            $table->float('colesterol_HDL',3,1);
+            $table->text('observacoes');
+            $table->unsignedBigInteger('user_id');
+            
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkups');
+    }
+}
