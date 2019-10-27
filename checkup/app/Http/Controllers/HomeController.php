a5e187d58a5b62f42148bba8476fa54fdeeefcc4
gmatheus66 <gmatheus66@hotmail.com>
2019-10-27 01:21:55 -0300

    GH-4 Redirecionamento com base no usuario que se loga

--- checkup/app/Http/Controllers/HomeController.php
+++ checkup/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //dd(Auth::user()->type);
+        if(Auth::user()->type == "medic"){
+            return view('homemed');
+        }
+        else{
+            return view('home');
+        }
     }
 }
