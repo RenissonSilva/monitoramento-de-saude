10f8ab85b5e8fe2af574b9cf1e213fe3c56c0b3d
gmatheus66 <gmatheus66@hotmail.com>
2019-10-28 18:18:32 -0300

    GH-8 Alteração na listagem do checkup

--- checkup/resources/views/checkup/visualizar.blade.php
+++ checkup/resources/views/checkup/visualizar.blade.php
@@ -19,37 +19,37 @@
 
                     {{ csrf_field() }}
                     <div class="form-group">
-                        <label for="data_checkup">Data do check-up:</label>  
-                        <input class="form-control" name="data_checkup" value="{{ $checkup->data_checkup }}" type="text" readonly>
-                    </div>  
+                        <label for="data_checkup">Data do check-up:</label>
+                        <input class="form-control" name="data_checkup" value="{{ date('d-m-Y  H:m:s', strtotime($checkup->data_checkup))}}" type="text" readonly>
+                    </div>
                     <div class="form-group">
-                        <label for="peso">Peso:</label>  
+                        <label for="peso">Peso:</label>
                         <input class="form-control" name="peso" type="text" value="{{ $checkup->peso }}" readonly>
-                    </div>  
+                    </div>
                     <div class="form-group">
-                        <label for="altura">Altura:</label>  
+                        <label for="altura">Altura:</label>
                         <input class="form-control" name="altura" value="{{ $checkup->altura }}" type="number" readonly>
-                    </div> 
+                    </div>
                     <div class="form-group">
-                        <label for="pressao">Pressão arterial:</label>  
-                        <input class="form-control" name="pressao" value="{{ $checkup->pressao }}" type="number" readonly>
-                    </div>  
+                        <label for="pressao">Pressão arterial:</label>
+                        <input class="form-control" name="pressao" value="{{ $checkup->pressao }}" type="text" readonly>
+                    </div>
                     <div class="form-group">
-                        <label for="glicose">Nível de glicose:</label>  
+                        <label for="glicose">Nível de glicose:</label>
                         <input class="form-control" name="glicose" value="{{ $checkup->glicose }}" type="number" readonly>
                     </div>
                     <div class="form-group">
-                        <label for="colesterol_LDL">Colesterol LDL:</label>  
+                        <label for="colesterol_LDL">Colesterol LDL:</label>
                         <input class="form-control" name="colesterol_LDL" value="{{ $checkup->colesterol_LDL }}" type="number" readonly>
-                    </div>  
+                    </div>
                     <div class="form-group">
-                        <label for="colesterol_HDL">Colesterol HDL:</label>  
+                        <label for="colesterol_HDL">Colesterol HDL:</label>
                         <input class="form-control" name="colesterol_HDL" value="{{ $checkup->colesterol_HDL }}" type="number" readonly>
-                    </div>  
+                    </div>
                     <div class="form-group">
-                        <label for="observacoes">Observações:</label>  
-                        <input class="form-control" name="observacoes" value="{{ $checkup->observacoes }}" type="number" readonly>
-                    </div>    
+                        <label for="observacoes">Observações:</label>
+                    <textarea class="form-control" name="observacoes" value="{{ $checkup->observacoes}}" type="number" readonly>{{ $checkup->observacoes}}</textarea>
+                    </div>
                 </div>
             </div>
         </div>
