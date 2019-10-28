bce3f76a26c3f7727c5bd689a24353e3ef2b286f
gmatheus66 <gmatheus66@hotmail.com>
2019-10-28 14:41:20 -0300

    GH-6 select para mostrar os pacientes

--- checkup/resources/views/checkup/adicionar.blade.php
+++ checkup/resources/views/checkup/adicionar.blade.php
@@ -7,14 +7,24 @@
         <div class="col-md-9">
             <div class="card">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item">Produtos</li>
+                        <li class="breadcrumb-item">Checkup</li>
                     </ol>
 
                 <div class="card-body">
                     <form action="{{route('salvar.checkup')}}" method="POST">
                         {{ csrf_field() }}
                         <div class="form-group">
-                            <label for="data_checkup">Data do check-up:</label>  
+                            <label for="patientselect">Paciente</label>
+                            <select class="form-control" id="patientselect" name="user_id">
+                                <option selected> Selecione um Paciente</option>
+                                @foreach ($patients as $patient)
+                                    <option value="{{$patient->id}}">{{$patient->name}}</option>
+
+                                @endforeach
+                            </select>
+                          </div>
+                        <div class="form-group">
+                            <label for="data_checkup">Data do check-up:</label>
                             <input type="date" value="{{old('data_checkup')}}" name="data_checkup" class="form-control"/>
                         </div>
                         @if($errors->any())
@@ -22,11 +32,11 @@
                                 <div class="alert alert-danger" role="alert">
                                   {{ $message }}
                                 </div>
-                            @endforeach 
+                            @endforeach
                         @endif
 
                         <div class="form-group">
-                            <label for="peso">Peso:</label>  
+                            <label for="peso">Peso:</label>
                             <input type="number" name="peso" class="form-control" value="{{old('peso')}}" step="0.01" placeholder="70.5"/>
                         </div>
                         @if($errors->any())
@@ -34,11 +44,11 @@
                                 <div class="alert alert-danger" role="alert">
                                   {{ $message }}
                                 </div>
-                            @endforeach 
+                            @endforeach
                         @endif
 
                         <div class="form-group">
-                            <label for="altura">Altura:</label>  
+                            <label for="altura">Altura:</label>
                             <input type="number" name="altura" class="form-control" value="{{old('altura')}}" step="0.01" placeholder="1.75"/>
                         </div>
                         @if($errors->any())
@@ -46,23 +56,23 @@
                                 <div class="alert alert-danger" role="alert">
                                   {{ $message }}
                                 </div>
-                            @endforeach 
+                            @endforeach
                         @endif
 
                         <div class="form-group">
-                            <label for="pressao">Pressão arterial:</label>  
-                            <input type="text" name="pressao" value="{{old('pressao')}}" class="form-control" placeholder="Elevada"/> 
+                            <label for="pressao">Pressão arterial:</label>
+                            <input type="text" name="pressao" value="{{old('pressao')}}" class="form-control" placeholder="Elevada"/>
                         </div>
                         @if($errors->any())
                             @foreach($errors->get('pressao') as $message)
                                 <div class="alert alert-danger" role="alert">
                                   {{ $message }}
                                 </div>
-                            @endforeach 
-                        @endif 
+                            @endforeach
+                        @endif
 
                         <div class="form-group">
-                            <label for="glicose">Nível de glicose:</label>  
+                            <label for="glicose">Nível de glicose:</label>
                             <input type="number" name="glicose" value="{{old('glicose')}}" class="form-control" placeholder="90"/>
                         </div>
                         @if($errors->any())
@@ -70,11 +80,11 @@
                                 <div class="alert alert-danger" role="alert">
                                   {{ $message }}
                                 </div>
-                            @endforeach 
-                        @endif 
+                            @endforeach
+                        @endif
 
                         <div class="form-group">
-                            <label for="colesterol_LDL">Colesterol LDL:</label>  
+                            <label for="colesterol_LDL">Colesterol LDL:</label>
                             <input type="number" name="colesterol_LDL" value="{{old('colesterol_LDL')}}" step="0.01" class="form-control" placeholder="120"/>
                         </div>
                         @if($errors->any())
@@ -82,11 +92,11 @@
                                 <div class="alert alert-danger" role="alert">
                                   {{ $message }}
                                 </div>
-                            @endforeach 
-                        @endif 
-                         
+                            @endforeach
+                        @endif
+
                         <div class="form-group">
-                            <label for="colesterol_HDL">Colesterol HDL:</label>  
+                            <label for="colesterol_HDL">Colesterol HDL:</label>
                             <input type="number" name="colesterol_HDL" value="{{old('colesterol_HDL')}}" step="0.01" class="form-control" placeholder="50"/>
                         </div>
                         @if($errors->any())
@@ -94,20 +104,20 @@
                                 <div class="alert alert-danger" role="alert">
                                   {{ $message }}
                                 </div>
-                            @endforeach 
-                        @endif 
+                            @endforeach
+                        @endif
 
                         <div class="form-group">
-                            <label for="observacoes">Observações:</label>  
-                            <input type="text" name="observacoes" value="{{old('observacoes')}}" class="form-control" placeholder="Ex: Paciente apresentou ..."/>
+                            <label for="observacoes">Observações:</label>
+                            <textarea type="text" name="observacoes" value="{{old('observacoes')}}" class="form-control" placeholder="Ex: Paciente apresentou ..." rows="3"></textarea>
                         </div>
                         @if($errors->any())
                             @foreach($errors->get('observacoes') as $message)
                                 <div class="alert alert-danger" role="alert">
                                   {{ $message }}
                                 </div>
-                            @endforeach 
-                        @endif 
+                            @endforeach
+                        @endif
 
                         <button type="submit" class="btn btn-success">Adicionar</button>
                     </form>
