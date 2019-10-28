c559af3f89b3031e45e902c147e49103efd3772b
gmatheus66 <gmatheus66@hotmail.com>
2019-10-28 17:44:45 -0300

    GH-8 Criação do arquivo para listagem do checkup para o usuario(paciente)

---
+++ checkup/resources/views/checkup/listaciente.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <div class="card">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item active">Check-ups</li>
+                </ol>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    @if(session('success'))
+                        <div class="alert alert-success" role="alert" id="sucessoCriacao"><strong>{{session('success')}}</strong></div>
+                    @endif
+
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                               <th>ID</th>
+                               <th>Nome do paciente</th>
+                               <th>Peso</th>
+                               <th>Altura</th>
+                               <th>Data do check-up</th>
+                               <th>Detalhes</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($checkups as $checkup)
+                            <tr>
+                                <th scope="row">{{ $checkup->id }}</th>
+                                <td>{{ $checkup->user->name}}</td>
+                                <td>{{ $checkup->peso }}</td>
+                                <td>{{ $checkup->altura }}</td>
+                                <td>{{ date('d-m-Y', strtotime($checkup->data_checkup))}}</td>
+                                <td>
+                                <a href="{{ route('visualizar.checkup', $checkup->id) }}" class="btn btn-success">Detalhes</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
