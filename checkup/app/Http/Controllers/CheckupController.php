a41092b66d00d95e3dbd2e72789dd77961590616
RenissonSilva <renissonx@gmail.com>
2019-10-27 21:22:07 -0300

    GH-2 Adição da função de listagem e salvar checkups

--- checkup/app/Http/Controllers/CheckupController.php
+++ checkup/app/Http/Controllers/CheckupController.php
@@ -10,8 +10,29 @@ class CheckupController extends Controller
     {
         $this->middleware('auth');
     }
+    public function listagemCheckup()
+    {
+        $checkups = \App\Checkup::paginate(5);
+        return view('checkup.listagem', compact('checkups'));
+    }
     public function adicionarCheckup()
     {
         return view('checkup.adicionar');
+    }
+    public function salvarCheckup(Request $request)
+    {
+        $request->validate([
+        	'data_checkup' => 'required',
+			'peso' => 'required',
+			'altura' => 'required',
+			'pressao' => 'required',
+			'glicose' => 'required',
+			'colesterol_LDL' => 'required',
+			'colesterol_HDL' => 'required',
+			'observacoes' => 'required'
+        ]);
+
+        \App\Checkup::create($request->all());
+        return redirect()->route('listagem.checkup');
     }
 }
