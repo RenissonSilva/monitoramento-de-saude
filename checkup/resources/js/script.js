b4c3d40e632090c2763ec901dbe58f51b8cef5da
GuiLira <liraguilherme252@gmail.com>
2019-10-29 13:24:12 -0300

    GH-2 Script para o input checkbox

--- checkup/resources/js/script.js
+++ checkup/resources/js/script.js
@@ -1,1 +1,17 @@
 console.log('run');
+$(document).ready(function(){
+    let check = $('#check');
+    let pass = $('#password');
+
+    check.on('click', function(evt){
+        console.log('click');
+        if(check.is(':checked')){
+            pass.get(0).type = 'text';
+        }
+        else{
+            pass.get(0).type = 'password';
+        }
+    })
+
+
+})
